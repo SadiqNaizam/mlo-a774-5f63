d6e1ffd73c51fc1ee61492f1e8b705139a84faf4
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-20 00:31:01 +0530

    Updated code files: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,33 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import LoginPage from "./pages/LoginPage";
 import NotFound from "./pages/NotFound";
+import { ThemeProvider } from "@/contexts/ThemeProvider"; // Added import
 
 const queryClient = new QueryClient();
 
 
 const App = () => (
-<QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-        <Routes>
+<ThemeProvider storageKey="authapp-theme" defaultTheme="light"> {/* Added ThemeProvider */}
+  <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+          <Routes>
 
 
-          <Route path="/" element={<LoginPage />} />
-          {/* catch-all */}
-          <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<LoginPage />} />
+            {/* catch-all */}
+            <Route path="*" element={<NotFound />} />
 
 
-        </Routes>
-    </BrowserRouter>
-    </TooltipProvider>
-</QueryClientProvider>
+          </Routes>
+      </BrowserRouter>
+      </TooltipProvider>
+  </QueryClientProvider>
+</ThemeProvider>
 );
 
 export default App;
